41f6626f3392437efb4a184ecc8b22e7ca3ad6aa
Mila <malaksumaaa@gmail.com>
2025-07-05 17:03:03 +0200

    add task end

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
 
+const formLogin = document.querySelector(".login-form")
+
+formLogin.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+
+    if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+   const formData = {
+    email,
+    password,
+  }});
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,21 @@
+const changeBody = document.body;
+const btn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener('click', colorChangeBody);
+
+function colorChangeBody() {
+  const newColor = getRandomHexColor();
+  
+  changeBody.style.backgroundColor = newColor;
+
+  colorSpan.textContent = newColor;
+  
+};
+
